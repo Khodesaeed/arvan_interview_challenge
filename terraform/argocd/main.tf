64eaf54fbc8b622d93405e1c99aab10e761e555e
Saeed <s.kazemi@arz.team>
2025-09-08 21:23:58 +0330

    Add argocd app of apps resource

--- terraform/argocd/main.tf
+++ terraform/argocd/main.tf
@@ -18,3 +18,34 @@ resource "helm_release" "argocd" {
     }
   ]
 }
+
+resource "kubernetes_manifest" "argocd_root_app" {
+  manifest = {
+    "apiVersion" = "argoproj.io/v1alpha1"
+    "kind"       = "Application"
+    "metadata" = {
+      "name"      = "root-app"
+      "namespace" = "argocd"
+      "finalizers" = ["resources-finalizer.argocd.argoproj.io"]
+    }
+    "spec" = {
+      "project" = "default"
+      "source" = {
+        "repoURL"        = "https://github.com/Khodesaeed/arvan_interview_challenge.git"
+        "targetRevision" = "main"
+        "path"           = "gitops/argo-apps" # this folder contains other Application manifests
+      }
+      "destination" = {
+        "server"    = "https://kubernetes.default.svc"
+        "namespace" = "argocd"
+      }
+      "syncPolicy" = {
+        "automated" = {
+          "prune"    = true
+          "selfHeal" = true
+        }
+      }
+    }
+  }
+  depends_on = [helm_release.argocd]
+}
