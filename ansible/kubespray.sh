2ca32c5a6e9e063d0e66463252c87a5c6c39784b
Saeed <s.kazemi@arz.team>
2025-09-08 21:02:00 +0330

    Update script

--- ansible/kubespray.sh
+++ ansible/kubespray.sh
@@ -8,4 +8,4 @@ docker run --rm -it \
   -v "$(pwd)/kubespray_cache":/tmp/kubespray_cache \
   -e "ANSIBLE_CONFIG=/kubespray/ansible.cfg" \
   quay.io/kubespray/kubespray:v2.28.1  \
-  ansible-playbook -i inventory/mycluster/hosts.yaml cluster.yml -e ansible_ssh_private_key_file=/root/.ssh/id_rsa -e host_key_checking=False --become --tags cilium 
+  ansible-playbook -i inventory/mycluster/hosts.yaml cluster.yml -e ansible_ssh_private_key_file=/root/.ssh/id_rsa -e host_key_checking=False --become  
